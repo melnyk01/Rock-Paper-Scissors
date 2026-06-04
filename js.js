1e80c7ac9c977d82320863174c993c4592438a41
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-04 15:02:36 +0200

    Write the logic to get computer and player choices Declare the players score variables Write a logic to play a single round

--- js.js
+++ js.js
@@ -1,13 +1,47 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
 getComputerChoice = function() {
     let num = Math.floor(Math.random() * 3);
-    return num;
+    let computerChoice = "";
+    switch (num) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    
+    return computerChoice;
 }
 
 getHumanChoice = function() {
-    const choice = Number(prompt("Please enter the number to play a Game: 1. Rock 2. Paper 3. Scissors"));
-    return choice;
+    const choice = prompt("Please enter the item to play the Game:  Rock,  Paper or Scissors");
+    console.log(choice);
+    return choice.toLowerCase();
 }
 
 console.log(getComputerChoice())
 getHumanChoice();
+
+playRound = function(humanChoice, computerChoice) {
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+playGame = function();
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
